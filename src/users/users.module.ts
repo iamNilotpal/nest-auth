e5a3b4fcb4ecd80893b657d360b9d23cbff675da
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-12-31 14:09:53 +0530

    Update user module and new column to user entity

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm/dist';
-import { User } from './entities/user.entity';
-import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService],
 })
