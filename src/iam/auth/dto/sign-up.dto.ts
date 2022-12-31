573033c182a6c5369df63cea4c66037c09c451f2
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-12-31 14:07:55 +0530

    Add signin and signup dtos

---
+++ src/iam/auth/dto/sign-up.dto.ts
@@ -0,0 +1,13 @@
+import { IsString, IsEmail, MinLength } from 'class-validator';
+
+export class SignUpDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(5)
+  password: string;
+}
