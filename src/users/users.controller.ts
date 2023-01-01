a0dac60d8033f49638a29b322db375bd105d5082
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2023-01-01 11:52:24 +0530

    Add implementations for user resource

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,25 +1,37 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Query,
+} from '@nestjs/common';
+import { FindByEmailDto } from './dto/find-by-email.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
-
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  async findAll(
+    @Query('skip') skip: number,
+    @Query('limit') limit: number,
+  ): Promise<User[]> {
+    return await this.usersService.findAll({ limit, skip });
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  async findOneById(@Param('id') id: number): Promise<User> {
+    return await this.usersService.findOneById(id);
+  }
+
+  @Get('by-email')
+  async findOneByEmail(@Body() emailDto: FindByEmailDto): Promise<User> {
+    return await this.usersService.findOneByEmail(emailDto.email);
   }
 
   @Patch(':id')
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,58 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PG_ERROR_CODES } from 'src/common/constants/pg.error-codes';
+import { FindOneOptions, Repository } from 'typeorm';
+import { PaginationDto } from './dto/pagination.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  private async findOne(filter: FindOneOptions<User>): Promise<User> {
+    return this.userRepository.findOne(filter);
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(paginationDto: PaginationDto): Promise<User[]> {
+    const { limit, skip } = paginationDto;
+    return this.userRepository.find({
+      skip,
+      take: limit <= 20 ? limit : 20,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOneById(id: number): Promise<User> {
+    const user = await this.findOne({ where: { id } });
+    if (!user) throw new NotFoundException(`User with id ${id} doesn't exist`);
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async findOneByEmail(email: string): Promise<User> {
+    const user = await this.findOne({ where: { email } });
+    if (!user)
+      throw new NotFoundException(`User with email ${email} doesn't exist`);
+    return user;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto): Promise<void> {
+    const { affected } = await this.userRepository.update(id, {
+      ...updateUserDto,
+    });
+    if (affected === PG_ERROR_CODES.NOT_AFFECTED)
+      throw new BadRequestException("User does't exist.");
+  }
+
+  async remove(id: number): Promise<void> {
+    const { affected } = await this.userRepository.delete({ id });
+    if (affected === PG_ERROR_CODES.NOT_AFFECTED)
+      throw new BadRequestException("User does't exist.");
   }
 }
